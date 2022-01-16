4de8fe162ac35c8e9ee2ebcb4ac0f422bc028af1
Ethan Lim <>
2022-01-16 00:34:58 +0000

    I am mildly confused

--- main.py
+++ main.py
@@ -101,12 +101,12 @@ api_key = "AIzaSyBCvBjjP32dgS6HrYVbNmVDAVEFswsjPIU"
 
 @client.command(aliases=["show"])
 async def showpic(ctx, *, search):
-    ran = random.randint(0, 9)
+    random_value = random.randint(0, 9)
     resource = build("customsearch", "v1", developerKey=api_key).cse()
     result = resource.list(q=f"{search}",
                            cx="b05c6ba6fc551be3d",
                            searchType="image").execute()
-    url = result["items"][ran]["link"]
+    url = result["items"][random_value]["link"]
     embed1 = discord.Embed(title=f"Here Is Your Image ({search.title()})")
     embed1.set_image(url=url)
     await ctx.send(embed=embed1)
